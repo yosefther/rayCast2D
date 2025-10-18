f6abaf30ba68c6a13463f57269cb65da9c312194
yosefther <yousefthair9@gamil.com>
2025-10-19 01:04:11 +0300

    Refactor main.py to improve player rendering and screen update logic

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import sys
 import pygame
 from map import Map
+from player import Player
 from settings import *
 screen = pygame.display.set_mode((WINDOW_WIDTH , WINDOW_HIGHT))
 map = Map()
+player = Player()
 while True :
     for event in pygame.event.get():
         if event.type == pygame.QUIT: 
             sys.exit()
     
+    screen.fill((0,0,0))
+    map.map_render(screen)
+    player.playr_render(screen)
 
-    screen.fill(0)
-    map.render(screen)
     pygame.display.update()
