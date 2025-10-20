d3d92c2fcd6900118b01f97e2ecc54b8616d8d3e
yosefther <yousefthair9@gamil.com>
2025-10-20 23:58:37 +0300

    Fix typo in main.py: correct WINDOW_HIGHT to WINDOW_HEIGHT and refactor player rendering call

--- main.py
+++ main.py
@@ -3,16 +3,20 @@ import pygame
 from map import Map
 from player import Player
 from settings import *
-screen = pygame.display.set_mode((WINDOW_WIDTH , WINDOW_HIGHT))
+screen = pygame.display.set_mode((WINDOW_WIDTH , WINDOW_HEIGHT))
 map = Map()
 player = Player()
+clock = pygame.time.Clock()
 while True :
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT: 
             sys.exit()
     
+    player.update()
+
     screen.fill((0,0,0))
     map.map_render(screen)
-    player.playr_render(screen)
-
+    player.player_render(screen)
+    
     pygame.display.update()
